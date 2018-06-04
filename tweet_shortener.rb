125a2f4f9f938dd2d2a090eb57c31a6c9390f34e
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-04 01:05:47 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def dictionary
-    short_cuts = {"hello" => "hi", "to, two, too" => "2", "for, four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+    short_cuts = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
     short_cuts
 end
 
